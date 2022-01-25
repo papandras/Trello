a1c1fd594640e9c6f7c8ba48e0320da60e74ddc2
papandras <papandras01@gmail.com>
2022-01-25 13:22:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ function kijelentkezes(){
     localStorage.setItem("auth", false);
     document.location.assign("login.html");
 }
+
+fetch('https://api.trello.com/1/actions/ab68071cce6d5327c51aff6ee120a7ed?text=3a03937377c536ac504197988cc3ed254084472039551a846481227f62e71b59').then(r => r.json());
