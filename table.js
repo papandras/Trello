7b341c009b0c5cecf67e27f716a6769878583bc2
farkaspeter14b <farkas.peter.55@gmail.com>
2022-02-01 14:48:31 +0100

    Feladtam...
    egy elkeseredett kísérlet a táblázat megszerkeztésére és megjelenítésér...

--- table.js
+++ table.js
@@ -1,7 +1,140 @@
 function newtable() {
-    document.getElementById("main-container").innerHTML = "<table id='table-first'></table>";
+    document.getElementById("main-container").innerHTML = "";
 }
 
 function newrow() {
-    document.getElementById("table-first").innerHTML = "<tr id='tr-first'><td>KKK</td></tr>";
+    document.getElementById("table-first").innerHTML = "<tr id='tr-first'> g</tr>";
 }
+
+function newcol() {
+    document.getElementById("table-first").innerHTML = "<tr id='td-first'>h</td>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="list js-list-content"><div class="list-header js-list-header u-clearfix is-menu-shown">
+        <div class="list-header-target js-editing-target"></div>
+            <h2 class="list-header-name-assist js-list-name-assist" dir="auto">Teendő</h2>
+                <textarea class="list-header-name mod-list-name js-list-name-input" aria-label="Teendő" spellcheck="false" dir="auto" maxlength="512" style="overflow: hidden; overflow-wrap: break-word; height: 28px;">Teendő</textarea>
+                    <div class="list-header-extras">
+                        <span class="list-header-extras-subscribe js-list-subscribed hide">
+                            <span class="icon-sm icon-subscribe mod-quiet"></span>
+                            </span>
+                            <span class="list-header-extras-limit-badge js-list-limit-badge hide">
+                                </span>
+                                <a class="list-header-extras-menu dark-hover js-open-list-menu icon-sm icon-overflow-menu-horizontal" href="#" aria-label="Műveletek listázása">
+                                    <div>
+                                        </div>
+                                        </a>
+                                        </div>
+                                        <p class="list-header-num-cards hide js-num-cards">0 kártya</p>
+                                        </div>
+                                        <div class="list-cards u-fancy-scrollbar u-clearfix js-list-cards js-sortable ui-sortable">
+                                            <div class="card-composer hide">
+                                                <div class="list-card js-composer">
+                        <div class="list-card-details u-clearfix">
+                            <div class="list-card-labels u-clearfix js-list-card-composer-labels">
+                        </div>
+                        <textarea class="list-card-composer-textarea js-card-title" dir="auto" placeholder="Adj címet ennek a kártyának..." style="overflow: hidden; overflow-wrap: break-word; height: 60px;">
+                            </textarea>
+                            <div class="list-card-members js-list-card-composer-members">
+                                </div>
+                                </div>
+                                </div>
+                                <div class="cc-controls u-clearfix">
+                                    <div class="cc-controls-section">
+                                        <input class="nch-button nch-button--primary confirm mod-compact js-add-card" type="submit" value="Új kártya">
+                                        <a class="icon-lg icon-close dark-hover js-cancel" href="#"></a>
+                                    </div>
+                                    <div class="cc-controls-section mod-right">
+                                    <a class="icon-lg icon-overflow-menu-horizontal dark-background-hover js-cc-menu" href="#">
+                                    </a>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="card-composer-container js-card-composer-container">
+                        <a class="open-card-composer js-open-card-composer" href="#">
+                        <span class="icon-sm icon-add"></span>
+                    <span class="js-add-a-card">Kártya hozzáadása</span>
+                    </a>
+                    <div class="js-card-templates-button card-templates-button-container dark-background-hover">
+                    <div class="js-react-root"><div role="presentation">
+                    <a class="LtbSQ1JzN14usb" data-test-id="card-template-list-button" role="button" href="#" aria-label="Létrehozás sablonból...">
+                    <span class="icon-sm icon-template-card dark-background-hover">
+                    </span>
+                </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+-->
