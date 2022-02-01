e0b7c13e5b3ff50641e87b154173a68e5668793f
farkaspeter14b <farkas.peter.55@gmail.com>
2022-02-01 14:01:25 +0100

    submit nélküli tabla

--- table.js
+++ table.js
@@ -1,4 +1,7 @@
 function newtable() {
-    let be = document.getElementById("main-container");
-    be.innerHTML = "<table id='table-first'></table>";
+    document.getElementById("main-container").innerHTML = "<table id='table-first'></table>";
+}
+
+function newrow() {
+    document.getElementById("table-first").innerHTML = "<tr id='tr-first'><td>KKK</td></tr>";
 }
