43ac1bdd70a0c714aa6c101a4934fa4c62bcee2c
papandras <papandras01@gmail.com>
2022-02-01 13:41:06 +0100

    bugfix: csukózárójel lecsípődött

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ if (JSON.parse(localStorage.getItem("auth")) == null) {
 
 if (JSON.parse(localStorage.getItem("auth")) == false) {
     document.location.assign("login.html");
-	
-if(JSON.parse(localStorage.getItem("auth")) == false){
-    //document.location.assign("login.html");
+}
+
+if (JSON.parse(localStorage.getItem("auth")) == false) {
+    document.location.assign("login.html");
 }
 
 function kijelentkezes() {
@@ -16,7 +17,8 @@ function kijelentkezes() {
 
 let tablak = Array();
 fetch(`https://api.trello.com/1/members/me/boards?key=ab68071cce6d5327c51aff6ee120a7ed&token=3a03937377c536ac504197988cc3ed254084472039551a846481227f62e71b59`)
-    .then(response => {response.json();})
+    .then(response => { response.json(); })
     .then(response => {
         tablak = response;
-    }).then(console.log("tablak: " + tablak)).catch(err => console.error(err));
+    }).then(console.log("tablak: " + tablak))
+    .catch(err => console.error(err));
